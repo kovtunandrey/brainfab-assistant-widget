ea459eb7bc1b5575b6aaafd037797f06c7787ff5
Ilya Rybakov <47.2region@gmail.com>
2024-07-11 14:10:15 +0600

    change visual style and add z-index values

--- widget_dev.js
+++ widget_dev.js
@@ -29,7 +29,55 @@ function assistant_widget(assistant_id) {
 
 function createAndAppendWidget(assistant_id) {
     const newElement = document.createElement('div');
-    newElement.innerHTML = `<style>#chat-widget{font-family:Verdana,Arial,Helvetica,sans-serif;background-color:#1a56db;position:fixed;bottom:20px;right:20px;width:60px;height:60px;padding:10px;color:#fff;border-radius:50%;display:flex;align-items:center;justify-content:center;cursor:pointer;text-align:center;font-size:13px}#chat-container{background-color:#fff;display:none;position:fixed;bottom:110px;right:20px;width:300px;height:600px;border:1px solid #ccc;border-radius:8px;overflow:hidden;padding:5px}#iframe-container{width:100%;height:100%}@media (max-width:765px){#chat-widget{padding:50px}}</style><body><div id="chat-widget" onclick="toggleChat()"><span>Brainfab Assistant</span></div><div id="chat-container"><div id="iframe-container"><iframe style="border:0px;height:100%" src="https://assistant.brainfab.com/version=5pml/widget-chat/${encodeURIComponent(assistant_id)}"></iframe></div></div>`;
+    newElement.innerHTML = `<style>
+    #chat-widget {
+        font-family: Verdana, Arial, Helvetica, sans-serif;
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+        width: 100px;
+        height: 100px;
+        cursor: pointer;
+        text-align: center;
+        font-size: 13px;
+        z-index: 10000;
+        background-image: url('https://be1338c21a36dfb47a79e72d7caa09ce.cdn.bubble.io/f1720523944703x543528083910456600/svgviewer-output.svg');
+        background-size: 95%; 
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+    #chat-container {
+        background-color: #fff;
+        display: none;
+        position: fixed;
+        bottom: 125px;
+        right: 20px;
+        width: 400px;
+        height: 600px;
+        border: 1px solid #ccc;
+        border-radius: 8px;
+        overflow: hidden;
+        padding: 5px;
+        z-index: 10000;
+    }
+    #iframe-container {
+        width: 100%;
+        height: 100%;
+    }
+    @media (max-width: 765px) {
+        #chat-widget {
+            padding: 50px;
+        }
+    }
+    </style>
+    <body>
+        <div id="chat-widget" onclick="toggleChat()"></div>
+        <div id="chat-container">
+            <div id="iframe-container">
+                <iframe style="border:0px; height:100%" src="https://assistant.brainfab.com/version-5pml/widget-chat/${encodeURIComponent(assistant_id)}"></iframe>
+            </div>
+        </div>
+    </body>`;
     document.body.appendChild(newElement);
 }
 
