5b100a71cf3613f1054a0d4f0ed87eb12c5b05b9
Ilya Rybakov <47.2region@gmail.com>
2024-07-23 22:46:10 +0600

    set mobile widget fullscreen size

--- widget.js
+++ widget.js
@@ -64,17 +64,48 @@ function createAndAppendWidget(assistant_id) {
     width: 100%;
     height: 100%;
 }
+#close-chat {
+    display: none;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    width: 30px;
+    height: 30px;
+    cursor: pointer;
+    font-size: 30px;
+    line-height: 30px;
+    text-align: center;
+    color: #000;
+    background-color: #fff;
+    border-radius: 50%;
+    z-index: 10001;
+}
 @media (max-width: 765px) {
     #chat-widget {
         width: 65px;
-        height: 65px;
+        height: 65px; 
         background-size: 90%;
+    }
+    #chat-container {
+        bottom: 0;  
+        right: 0;
+        left: 0;
+        top:0;
+        width: 100%;
+        height: calc(100%);
+        border-radius: 0;
+        border: 0 px solid #ccc;
+        z-index: 10001;
+    }
+    #close-chat {
+        display: block;
     }
 }
 </style>
 <body>
     <div id="chat-widget" onclick="toggleChat()"></div>
     <div id="chat-container">
+        <div id="close-chat" onclick="toggleChat()">×</div>
         <div id="iframe-container">
             <iframe style="border:0px; width: 100%; height:100%" src="https://assistant.brainfab.com/widget-chat/${encodeURIComponent(assistant_id)}"></iframe>
         </div>
@@ -85,5 +116,15 @@ function createAndAppendWidget(assistant_id) {
 
 function toggleChat() {
     const chatContainer = document.getElementById("chat-container");
-    chatContainer.style.display = (chatContainer.style.display === "none" || chatContainer.style.display === "") ? "block" : "none";
+    const body = document.body;
+    const html = document.documentElement;
+    if (chatContainer.style.display === "none" || chatContainer.style.display === "") {
+        chatContainer.style.display = "block";
+        body.style.overflow = "hidden";
+        html.style.overflow = "hidden";
+    } else {
+        chatContainer.style.display = "none";
+        body.style.overflow = "auto";
+        html.style.overflow = "auto";
+    }
 }
